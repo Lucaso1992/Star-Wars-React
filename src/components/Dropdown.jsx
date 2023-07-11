ee3b0d2f499d6c418293b421f1dcae12a5587097
lucas solari <solarilucas92@gmail.com>
2023-07-11 17:24:44 +0200

    favorites

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom"
 import useAppContext from "../context/AppContext";
 
-const Dropdown = ({ favorites }) => {
+const Dropdown = () => {
     const { store } = useAppContext();
     return (
         <div className="dropdown me-5">
             <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Favorites
+                Favorites 
             </button>
             <ul className="dropdown-menu dropdown-menu-end">
             {store.favorites.map((element, index) => {
-                return (<li key={index} ><div className="dropdown-item" >
+                return (<li key={index} ><div className="dropdown-item p-2" >
                     <Link to={`http://localhost:5173/detail/${element.type}/${element.id}`}>{element.name}</Link></div></li>);
             })
             }</ul>
